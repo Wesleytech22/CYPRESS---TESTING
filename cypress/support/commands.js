1ecd330a8a19bd13d786f96cd9177487c8134d62
Wesley Rodrigues <wesley.rodrigues@amazonasinovare.com.br>
2026-04-16 18:42:30 -0300

    feat: :sparkles: logout implemented

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,9 +1,24 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
+Cypress.Commands.add('login', (environment = 'production') => {
+    cy.fixture('environments.json').then((environments) => {
+        const config = environments[environment]
+        cy.visit(config.url)
+        cy.get('[name="username"]').type(config.users.validUser.usernameValid)
+        cy.get('[name="password"]').type(config.users.validUser.passwordValid)
+        cy.get('.MuiButton-root').click()
+        cy.title().should('not.include', '/login')
+    })
+})
+
+Cypress.Commands.add('logout', () => {
+
+    cy.get('.MuiIconButton-root').click()
+    cy.wait(500)
+    cy.get('.MuiStack-root > :nth-child(4)')
+        .should('be.visible')
+        .click()
+    cy.wait(500)
+    cy.get(':nth-child(4) > .MuiListItemText-root > .MuiTypography-root')
+
+    cy.url().should('include', '/login')
+    cy.title().should('include', 'MDM-Hub')
+})
