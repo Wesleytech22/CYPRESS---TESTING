95ad1608df06afb0928b1e8558b9fe230f4aa5ea
Wesley Rodrigues <wesley.rodrigues@amazonasinovare.com.br>
2026-04-17 10:33:42 -0300

    performance: :zap: better performance of login method calls

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,18 +1,7 @@
 describe('Login Test', () => {
     let environment = Cypress.env('environment') || 'production'
-    let environmentConfig
-
-    beforeEach(() => {
-        cy.fixture('environments.json').then((environments) => {
-            environmentConfig = environments[environment]
-        })
-    })
 
     it('login to portal', () => {
-        cy.visit(environmentConfig.url)
-        cy.title().should('include', 'MDM-Hub')
-        cy.get('[name="username"]').type(environmentConfig.users.validUser.usernameValid)
-        cy.get('[name="password"]').type(environmentConfig.users.validUser.passwordValid)
-        cy.get('.MuiButton-root').click()
+        cy.loginAndGetConfigProd(environment)
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,11 +1,79 @@
-Cypress.Commands.add('login', (environment = 'production') => {
-    cy.fixture('environments.json').then((environments) => {
-        const config = environments[environment]
-        cy.visit(config.url)
-        cy.get('[name="username"]').type(config.users.validUser.usernameValid)
-        cy.get('[name="password"]').type(config.users.validUser.passwordValid)
+describe('Login Test', () => {
+    let environment = Cypress.env('environment') || 'production'
+    let environmentConfig
+
+    beforeEach(() => {
+        cy.fixture('environments.json').then((environments) => {
+            environmentConfig = environments[environment]
+        })
+    })
+
+    it('login to portal', () => {
+        cy.visit(environmentConfig.url)
+        cy.title().should('include', 'MDM-Hub')
+        cy.get('[name="username"]').type(environmentConfig.users.validUser.usernameValid)
+        cy.get('[name="password"]').type(environmentConfig.users.validUser.passwordValid)
         cy.get('.MuiButton-root').click()
-        cy.title().should('not.include', '/login')
+    })
+})
+
+// Comando de login com retorno da configuração (qa)
+Cypress.Commands.add('loginGetConfigQA', (environment = 'qa') => {
+    return cy.fixture('environments.json').then((environments) => {
+        const environmentConfigQA = environments[environment]
+
+        cy.visit(environmentConfigQA.url)
+        cy.title().should('include', 'MDM-Hub')
+        cy.get('[name="username"]').type(environmentConfigQA.users.validUser.usernameValid)
+        cy.get('[name="password"]').type(environmentConfigQA.users.validUser.passwordValid)
+        cy.get('.MuiButton-root').click()
+
+        return cy.wrap(environmentConfigQA)
+    })
+})
+
+// Comando de login com retorno da configuração (dev)
+Cypress.Commands.add('loginGetConfigDev', (environment = 'dev') => {
+    return cy.fixture('environments.json').then((environments) => {
+        const environmentConfigDev = environments[environment]
+
+        cy.visit(environmentConfigDev.url)
+        cy.title().should('include', 'MDM-Hub')
+        cy.get('[name="username"]').type(environmentConfigDev.users.validUser.usernameValid)
+        cy.get('[name="password"]').type(environmentConfigDev.users.validUser.passwordValid)
+        cy.get('.MuiButton-root').click()
+
+        return cy.wrap(environmentConfigDev)
+    })
+})
+
+// Comando de login com retorno da configuração (hml)
+Cypress.Commands.add('loginGetConfigHml', (environment = 'hml') => {
+    return cy.fixture('environments.json').then((environments) => {
+        const environmentConfigHml = environments[environment]
+
+        cy.visit(environmentConfigHml.url)
+        cy.title().should('include', 'MDM-Hub')
+        cy.get('[name="username"]').type(environmentConfigHml.users.validUser.usernameValid)
+        cy.get('[name="password"]').type(environmentConfigHml.users.validUser.passwordValid)
+        cy.get('.MuiButton-root').click()
+
+        return cy.wrap(environmentConfigHml)
+    })
+})
+
+// Comando de login com retorno da configuração (production)
+Cypress.Commands.add('loginAndGetConfigProd', (environment = 'production') => {
+    return cy.fixture('environments.json').then((environments) => {
+        const environmentConfigProd = environments[environment]
+
+        cy.visit(environmentConfigProd.url)
+        cy.title().should('include', 'MDM-Hub')
+        cy.get('[name="username"]').type(environmentConfigProd.users.validUser.usernameValid)
+        cy.get('[name="password"]').type(environmentConfigProd.users.validUser.passwordValid)
+        cy.get('.MuiButton-root').click()
+
+        return cy.wrap(environmentConfigProd)
     })
 })
 
